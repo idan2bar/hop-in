6a442e239c9223bc0a0656ff8231c94db9bb221a
Idan Bar <idan2bar@gmail.com>
2025-03-20 23:10:15 +0200

    Created the page structure with example link buttons

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 import van from "./van-1.5.3.min.js";
 
-const { div, button } = van.tags;
-const { state, add } = van;
+const { div, button, img, h2 } = van.tags;
+const { add } = van;
 
-const count = state(0);
+const links = [
+    { name: "Google", url: "https://www.google.com/" },
+    { name: "Facebook", url: "https://www.facebook.com/" },
+    { name: "X", url: "https://x.com/" },
+]
 
-const Counter = () =>
+const GetFaviconUrl = (url, size=64) =>
+    `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}&sz=${size}`;
+
+const Page = () =>
     div(
-        { class: "counter" },
-        "Count: ", count,
-        button({ onclick: () => ++count.val }, "Increment")
-    );
+        { class: "page" },
+        Navbar(),
+        Content(),
+    )
 
-add(document.body, Counter());
+const Navbar = () =>
+    div(
+        { class: "navbar" },
+        h2("Hop In!")
+    )
+
+const Content = () => div(
+    { class: "content" },
+    LinkButtons()
+);
+
+const LinkButtons = () => div(
+    { class: "link-buttons-container" },
+    links.map(({ name, url }) =>
+        button(
+            {class: "link-button", onclick: () => window.location.href = url},
+            img({src: GetFaviconUrl(url), alt: `${name} icon`, class: "icon"}),
+            name
+        )
+    )
+)
+
+add(document.body, Page());
