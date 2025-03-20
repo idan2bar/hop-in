8c3b5fe218fefdadba3ca23ad19b499ea5ae2517
Idan Bar <idan2bar@gmail.com>
2025-03-20 23:41:43 +0200

    Added the plus button

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
 import van from "./van-1.5.3.min.js";
 
-const { div, button, img, h2 } = van.tags;
-const { add } = van;
+const {div, button, img, h2} = van.tags;
+const {add} = van;
 
 const links = [
-    { name: "Google", url: "https://www.google.com/" },
-    { name: "Facebook", url: "https://www.facebook.com/" },
-    { name: "X", url: "https://x.com/" },
+    {name: "Google", url: "https://www.google.com/"},
+    {name: "Facebook", url: "https://www.facebook.com/"},
+    {name: "X", url: "https://x.com/"},
 ]
 
-const GetFaviconUrl = (url, size=64) =>
+const GetFaviconUrl = (url, size = 64) =>
     `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}&sz=${size}`;
 
 const Page = () =>
     div(
-        { class: "page" },
+        {class: "page"},
         Navbar(),
         Content(),
     )
 
 const Navbar = () =>
     div(
-        { class: "navbar" },
+        {class: "navbar"},
         h2("Hop In!")
     )
 
 const Content = () => div(
-    { class: "content" },
+    {class: "content"},
     LinkButtons()
 );
 
 const LinkButtons = () => div(
-    { class: "link-buttons-container" },
-    links.map(({ name, url }) =>
+    {class: "link-buttons-container"},
+    links.map(({name, url}) =>
         LinkButton(name, url)
-    )
+    ),
+    AddLinkButton()
+)
+
+const AddLinkButton = () => button(
+    {class: "link-button-body", onclick: () => alert('Add link clicked')},
+    h2("+")
 )
 
 const LinkButton = (name, url) => {
